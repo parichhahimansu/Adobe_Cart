b163366ec9b2b5222f8ab0623622977e8325b7a0
Himansu parichha <parichhahimansu@gmail.com>
2020-03-27 20:04:55 +0530

    Fixes

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -16,15 +16,17 @@ class Summary extends Component{
         
         let discountTotal = this.props.addedItems.map(amount).reduce(sum);
         let netAmount = this.props.total - discountTotal;
+        let totalItems = this.props.addedItems.length;
+        let totalString = totalItems > 1 ? `(${totalItems} Items)` : `(${totalItems} Item)`;
         return(
                 <div className="collection">
                     <li className="collection-item"><b>Price details</b></li>
                     <li className="collection-item">
-                        <p><b>Price: {this.props.total}$</b>&nbsp;&nbsp;&nbsp;&nbsp;</p><br/>
-                        <p><b>Discount: {discountTotal}$</b></p>
-                    </li>
+                        <b><span className="cart-det-item">Price {totalString}</span>: <span className="cart-det-price">{this.props.total}$</span></b></li>
                     <li className="collection-item">
-                        <b>Total: {netAmount}$</b>
+                        <b><span className="cart-det-item">Discount</span>: <span className="cart-det-price">{discountTotal}$</span></b></li>
+                    <li className="collection-item">
+                        <b><span className="cart-det-item">Total</span>: <span className="cart-det-price">{netAmount}$</span></b>
                     </li>
                 </div>
         )
